2efe837089d73f639758fbb5ed03f6654ffb5fde
sanemat <o.gata.ken@gmail.com>
2014-06-29 21:11:15 +0900

    Add files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+var paths = {
+  lint: ['./gulpfile.js', './app/index.js'],
+  watch: ['./gulpfile.js', './app/index.js', './test/**/*.js', '!test/{temp,temp/**}'],
+  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
+  source: ['./app/index.js']
+};
+
+gulp.task('lint', function () {
+  return gulp.src(paths.lint)
+    .pipe($.plumber())
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jscs())
+    .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('istanbul', function (cb) {
+  gulp.src(paths.source)
+    .pipe($.istanbul()) // Covering files
+    .on('finish', function () {
+      gulp.src(paths.tests, {cwd: __dirname})
+        .pipe($.plumber())
+        .pipe($.mocha())
+        .pipe($.istanbul.writeReports()) // Creating the reports after tests runned
+        .on('finish', function() {
+          process.chdir(__dirname);
+          cb();
+        });
+    });
+});
+
+gulp.task('watch', ['test'], function () {
+  gulp.watch(paths.watch, ['test']);
+});
+
+gulp.task('default', ['test']);
+gulp.task('test', ['lint', 'istanbul']);
